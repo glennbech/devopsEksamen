f38a7b32b8e0dd1d1d2c39ddfef732a4c4f4af0e
bjarte <22171847+freebattie@users.noreply.github.com>
2023-11-15 20:19:15 +0100

    Oppgave3B Fixed cpu and memory settings

--- infra/main.tf
+++ infra/main.tf
@@ -4,8 +4,8 @@ resource "aws_apprunner_service" "service" {
 
   instance_configuration {
     instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
-    cpu = 256
-    memory = 1024
+    cpu = "256 vCPU"
+    memory = "1024 MB"
   }
 
   source_configuration {
