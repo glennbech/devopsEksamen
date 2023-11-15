07a0857c50bf683e4664e64a987de2385d4913a1
bjarte <22171847+freebattie@users.noreply.github.com>
2023-11-15 23:01:48 +0100

    Oppgave3B update main with rights

--- infra/main.tf
+++ infra/main.tf
@@ -4,8 +4,8 @@ resource "aws_apprunner_service" "service" {
 
 
   instance_configuration {
-    cpu                   = "1 vCPU"
-    memory                = "2 GB"
+    cpu                   = "0.25 vCPU"
+    memory                = "1 GB"
     instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
 
   }
